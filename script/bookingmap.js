540b28676353feb8c5d4a815d4e5b58bf8d4be1e
stening <stening.johan@gmail.com>
2015-10-08 14:40:55 +0000

    jobbar med bokningskartan

--- script/bookingmap.js
+++ script/bookingmap.js
@@ -5,30 +5,68 @@ var boxnumberid = 1;
 var boxes = {};
 window.onload = function() {
     function generateseatrow(xstart, ystart, numberofseats) {
-        for (var i = 0; i < numberofseats; i++) {               //5an är antalet platser
-            boxes["box" + i] = map.getContext("2d");
-            boxes["box" + i].beginPath();
-            boxes["box" + i].fillStyle = "black";
-            boxes["box" + i].rect(xstart, ystart, 30, 30);
-            boxes["box" + i].lineWidth = 3;
-            boxes["box" + i].strokeStyle = 'black';
-            boxes["box" + i].font = "14pt Arial";
-            boxes["box" + i].fillText(boxnumberid, xstart + 5, ystart + 20);
-            boxes["box" + i].stroke();
+        for (var i = 0; i < numberofseats; i++) {
+            var box = map.getContext("2d");
+            boxes["box" + boxnumberid] = [xstart, ystart, xstart + 30, ystart + 30, boxnumberid];
+            box.beginPath();
+            box.fillStyle = "black";
+            box.rect(xstart, ystart, 30, 30);
+            box.lineWidth = 3;
+            box.strokeStyle = 'black';
+            box.font = "14pt Arial";
+            box.fillText(boxnumberid, xstart + 5, ystart + 20);
+            box.stroke();
             boxnumberid += 1;
-            xstart += 55;
+            xstart += 35;
             ystart += 0;
         }
     }
-    generateseatrow(10, 5, 5);
+    generateseatrow(10, 30, 5);
+    generateseatrow(40, 60, 5);
+    generateseatrow(80, 90, 5);
+    generateseatrow(100, 120, 5);
+    generateseatrow(130, 150, 5);
+    
+    
     console.log(boxes);
-    boxes.onclick = function(){
-    alert("box 2 clicked");
-    }
-    map.onclick = function(){
+    
+    
+    
+    
+    map.addEventListener("mousedown", function(e){
+        var totalOffsetX = 0;
+        var totalOffsetY = 0;
+        var canvasX = 0;
+        var canvasY = 0;
+        totalOffsetX += map.offsetLeft - map.scrollLeft;
+        totalOffsetY += map.offsetTop - map.scrollTop;
+        canvasX = e.pageX - totalOffsetX;
+        canvasY = e.pageY - totalOffsetY;
+        console.log(canvasX, ", ", canvasY);
+        
+    });
+    
+    
+    
+    
+    /*
+    map.onclick = function(e){
         
         
-    }
+        
+        
+        
+    }*/
+            /*var xclick = map.offsetx;
+            var yclick = map.offsety;
+            xclick -= map.offsetLeft;
+            yclick -= map.offsetTop;
+            alert("x:" + xclick + " y:" + yclick);
+            console.log(e.offsetLeft)
+            console.log(map.offsetTop)
+            console.log(xclick)
+            console.log(e.clientLeft)
+        }*/
     /*        for (var j = 0; j < i.length; j++) {
     map.getContext("2d");
     testbox.fillStyle = "green";
